214b2556dd38891cd31f7a8fbff07a56a26fae9f
sigales96 <73656611+sigales96@users.noreply.github.com>
2020-10-29 09:24:34 -0600

    Add files via upload

---
+++ Reto 1-Búsqueda de patrones mediante LIKE.sql
@@ -0,0 +1,15 @@
+#Reto 1: Búsqueda de patrones mediante LIKE
+#Bryand Javier Garcia Sigales
+USE tienda;
+SHOW TABLES;
+SELECT* FROM articulo;
+#¿Qué artículos incluyen la palabra Pasta en su nombre?
+SELECT * FROM articulo WHERE nombre LIKE "%PASTA%" ;
+#¿Qué artículos incluyen la palabra Cannelloni en su nombre?
+SELECT * FROM articulo WHERE nombre LIKE "%Cannelloni%";
+#¿Qué nombres están separados por un guión (-) por ejemplo Puree - Kiwi?
+SELECT * FROM articulo WHERE nombre LIKE "%-%";
+#¿Qué puestos incluyen la palabra Designer?
+SELECT * FROM puesto WHERE nombre LIKE "%Designer%";
+#¿Qué puestos incluyen la palabra Developer?
+SELECT * FROM puesto WHERE nombre LIKE "%Developer%";
