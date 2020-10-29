00ab7b67f97896c9e7356f5458ed909809cf9f9f
sigales96 <73656611+sigales96@users.noreply.github.com>
2020-10-29 09:29:13 -0600

    Update Reto 3- Agrupamientos.sql

--- Reto 3- Agrupamientos.sql
+++ Reto 3- Agrupamientos.sql
@@ -9,4 +9,4 @@ SELECT nombre, sum(salario) FROM puesto GROUP BY nombre;
 #¿Cuál es el número total de ventas por vendedor?
 SELECT id_empleado, count(clave) AS ventas FROM venta GROUP BY id_empleado;
 #¿Cuál es el número total de ventas por artículo?
-SELECT id_articulo, count(*) FROM venta GROUP BY id_articulo
+SELECT id_articulo, count(*) FROM venta GROUP BY id_articulo;
